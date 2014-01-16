83bbab1d43e8fe870323bcd5b198ea1617c57eda
BeanHsiang <xiangbin731@qq.com>
2014-01-16 16:20:04 +0800

    clear keydown event for input text

--- src/toneshot.js
+++ src/toneshot.js
@@ -1,6 +1,4 @@
-var re_quanpin = /(.+)(?=1)/g;
-var re_shouzimu = /(.+)(?=2)/g;
-var re_content = /[^\u4e00-\u9fa5a-zA-Z1-2]/g;
+var re_content = /[^\u4e00-\u9fa5a-zA-Z]/g;
 var pinyin = new Pinyin();
 var timeHtml = '<div>服务器时间：<span style="font-size: 20px;margin-left: 10px;border:3px solid #FF6701" data-bind="text: currentFormatTime"></span><span style="font-size: 14px;margin-left: 15px" data-bind="visible: useTime() > 0"><span data-bind="text: useTime"></span>s</span></div>';
 var localTime, serverTime, diffTime, intTimer;
@@ -48,18 +46,9 @@ function handleCaptcha() {
                 ans = ans.replace(re_content, "");
                 $(answer).val(ans);
             }
-            if (re_quanpin.test(ans)) {
-                var oldanswer = ans.match(re_quanpin)[0];
-                $(answer).val(pinyin.getFullChars(oldanswer).toLowerCase());
-                btn.click();
-            } else if (re_shouzimu.test(ans)) {
-                var oldanswer = ans.match(re_shouzimu)[0];
-                $(answer).val(pinyin.getCamelChars(oldanswer).toLowerCase());
-                btn.click();
-            }
         });
         $(answer).unbind("keydown").bind("keydown", function (e) {
-//                e.stopPropagation();
+            e.stopPropagation();
             if (e.keyCode == 13) {
                 btn.click();
                 endTime = new Date();
@@ -74,6 +63,17 @@ function handleCaptcha() {
 //                    } else {
 
 //                    }
+                return false;
+            }
+            var ans = $(answer).val();
+            if (e.keyCode == 49) {
+                $(answer).val(pinyin.getFullChars(ans).toLowerCase());
+                btn.click();
+                return false;
+            } else if (e.keyCode == 50) {
+                $(answer).val(pinyin.getCamelChars(ans).toLowerCase());
+                btn.click();
+                return false;
             }
         });
 //            $(answer).bind("keyup keypress", function (e) {
